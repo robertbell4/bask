cb41a6589a4869f89214490e4aa1ef49ffe6bc5c
Robert Bell <robertalexanderbell.iv@gmail.com>
2023-11-03 17:06:28 -0700

    started up main

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bask
 lizard sunning game
+
+This is my first attempt at any game development. 
+Will start by making something run in the terminal, work up from there.
+
+This will be the life of a lizard surviving through a nuclear winter.
+Main goal of the game will be to compete for warmth.
--- main.cpp
+++ main.cpp
@@ -1,1 +1,51 @@
 
+
+#include <iostream>
+
+void initializeGame();
+void refreshVis();
+
+
+
+void initializeGame(){
+
+
+}
+
+
+
+void refreshVis(){
+
+    for ( int i = 0; i < 10; i++){
+        if (  (i == 0) || (i== 9)  )
+        {
+            std::cout << "******************************      \n" << std::endl;
+        }
+        
+        else{
+            std::cout << "                                    \n" << std::endl;
+        }
+    }
+    
+    
+}
+
+
+
+
+int main(){
+
+    initializeGame();
+
+
+    int run = 10;
+    while(run){
+
+
+        refreshVis();
+        run--;
+        
+       }
+
+    return 0;
+}
